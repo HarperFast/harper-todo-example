729f43fe2cace8f2384567a949405394eba35849
Austin Akers <austin@harperdb.io>
2026-06-29 14:13:47 -0400

    updated start/ resoruces.js

--- start/resources.js
+++ start/resources.js
@@ -1,15 +1,2 @@
 // Here we can define any JavaScript-based resources and extensions to tables
 // Importing the tables available to us from the harper instance
-import { tables } from 'harper';
-
-const TodoListTable = tables.TodoList;
-
-export class TodoListResource extends TodoListTable {
-  static async put(target, taskItemData) {
-    return super.put(target, taskItemData);
-  }
-
-  static async post(target, taskItemData) {
-    return super.post(target, taskItemData);
-  }
-}
